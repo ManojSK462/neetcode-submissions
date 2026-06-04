9aff3d788c2ed5136d4eef99ee94660e0bf5d418
Sai Manoj Konidana <saimanojkonidana462@gmail.com>
2026-06-04 10:49:41 -0500

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.go
@@ -0,0 +1,18 @@
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {return false}
+
+	// counts := make(map[rune]int)
+	counts := [26]int{}
+
+	for i := 0; i < len(s); i++ {
+        counts[s[i]-'a']++
+        counts[t[i]-'a']--
+    }
+
+	for _, c := range counts {
+        if c != 0 {
+            return false
+        }
+	}
+return true
+}
