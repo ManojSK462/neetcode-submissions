771ee40651c682504028a403d4699dcbb55dfea1
Sai Manoj Konidana <saimanojkonidana462@gmail.com>
2026-08-20 23:43:44 -0500

    Add: lemonade-change - submission-6

---
+++ Data Structures & Algorithms/lemonade-change/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        denoms = {5:0, 10:0, 20:0}
+        for i in bills:
+            denoms[i]+=1
+            if i==10:
+                if denoms[5]==0:
+                    return False
+                denoms[5]-=1
+            elif i==20:
+                if denoms[5]==0:
+                    return False
+                else:
+                    if denoms[10]==0:
+                        if denoms[5]>=3:
+                            denoms[5]-=3
+                        else:
+                            return False
+                    else:
+                        denoms[5]-=1
+                        denoms[10]-=1
+
+        return True
+
+        
